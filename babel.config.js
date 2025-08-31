02b3fc4ac34c5787ccdc735c7f3afc253d862ed8
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-08-31 12:31:20 +0900

    feat : svg, native 윈드 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel"],
     plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["."],
-          alias: {
-            "@": ".",
-            "@features": "./features",
-            "@shared": "./shared",
-          },
+      ["module-resolver", {
+        root: ["."],
+        alias: {
+          "@": "./",
+          "@features": "./features",
+          "@shared": "./shared",
         },
-      ],
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+      }],
       "react-native-reanimated/plugin",
     ],
   };

15d27449a56b9c8d6fc1bb64cf5f1ab734e5bd7f
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-08-31 16:33:05 +0900

    chore : 주석 추가

--- components/useClientOnlyValue.ts
+++ components/useClientOnlyValue.ts
@@ -1,4 +1,4 @@
-// 웹 SSR 대비: 클라이언트에서만 true가 되도록
+// 웹 SSR 대비: 클라이언트에서만 true가 되도록 (삭제하셔도 됩니다.)
 import { useEffect, useState } from 'react';
 
 export function useClientOnlyValue<T>(server: T, client: T) {

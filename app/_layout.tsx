be4b25e8c32f9566a4c6c67e8c6dd53f93813d00
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-09-06 23:02:59 +0900

    feat : login page complete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 // app/(tabs)/_layout.tsx - 로그인 탭 숨기기(선택)
 import { Tabs } from 'expo-router';
+import 'global.css'
+
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="home" options={{ title: 'Home' }} />
-      <Tabs.Screen name="login" options={{ href: null, headerShown: false }} />
+      <Tabs.Screen name="login" options={{ href: null, tabBarStyle: { display: 'none' } }} />
     </Tabs>
   );
 }
